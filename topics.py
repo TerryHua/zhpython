010444c7addf328bf5421b239527401370136bae
huaxiaofeng <huaxiaofeng@wanshifu.com>
2016-04-29 18:07:02 +0800

    抓取修改

--- topics.py
+++ topics.py
@@ -6,30 +6,26 @@ import urllib.request
 import urllib.parse
 import json
 import re
+import html
+
 
 class ZhihuTopic:
     topUrl = "https://www.zhihu.com/topics"
 
     def __init__(self):
         self.userAgent = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36'}
-        self.requestUrl = 'http://translate.google.com.hk/translate_a/t'
+        self.requestUrl = 'http://www.baidu.com'
         self.requestMethod = 'POST'
 
     def getTopContent(self):
-        '''
-        request = urllib.Request(self.topUrl)
-        response = urllib.urlopen(request)
-        html = response.read()
-        response.close()
-        '''
         req = urllib.request.Request(self.topUrl)
         response = urllib.request.urlopen(req)
-        html = response.read()
-        return html
+        htmlText = response.read()
+        return htmlText
 
-    def getTopSign(self, html):
-        html = html.decode('UTF-8')
-        searchObj = re.search(r'zg-icon-topic-square(.*)zm-topic-cat-sub', html, re.M|re.I|re.S)
+    def getTopSign(self, htmlText):
+        htmlText = htmlText.decode('UTF-8')
+        searchObj = re.search(r'zg-icon-topic-square(.*)zm-topic-cat-sub', htmlText, re.M|re.I|re.S)
         if searchObj:
             hrefHtml = searchObj.group();
             urls = re.findall(r'<a.*?href="(.*?)">.*?<\/a>',hrefHtml,re.I)
@@ -39,10 +35,6 @@ class ZhihuTopic:
                 print ('this is over')
         else:
            print ("Nothing found!!")
-
-    def test11(self):
-        print('hello world')
-
 
     def getTopList(self, topicId):
         url = "https://www.zhihu.com/node/TopicsPlazzaListV2"
@@ -55,11 +47,40 @@ class ZhihuTopic:
         print(textDestination)
 
 
+    def getTopSignList(self, htmlText):
+        #通过ajax获取话题内的标签
+        htmlText = htmlText.decode('UTF-8')
+        offSet = '0'
+        xsrf = re.findall(r'name=\"_xsrf\" value=\"(.*)\"',htmlText,re.I)
+        #获取请求参数
+        param = re.findall(r'class=\"zh-general-list clearfix\" data-init=\"(.*)\">',htmlText,re.I)
+        paramText =  html.unescape(param[0])
+
+        paramDict = json.loads(paramText)
+
+        #url组装
+        url = "https://www.zhihu.com/node/"+paramDict['nodename']
+        values = {'method':'next','params': '{"topic_id":'+ str(paramDict['params']['topic_id']) + ',"offset":'+offSet+',"hash_id":"'+str(paramDict['params']['hash_id'])+'"}','_xsrf':xsrf[0]}
+
+        data = urllib.parse.urlencode(values).encode('UTF-8')
+        request = urllib.request.Request(url = url, data = data, headers = self.userAgent, method = self.requestMethod)
+
+        textDestination = urllib.request.urlopen(request).read().decode('utf-8');
+        textDestinationDict = json.loads(textDestination)
+        if textDestinationDict['msg']:
+            for textRow in textDestinationDict['msg']:
+                print (textRow)
+
+    def getSignLink(self, htmlText):
+        #获取单个话题的链接地址
+        link = re.findall(r'<a target=\"_blank\" href=\"(.*)\">',htmlText,re.I)
+        url = "https://www.zhihu.com/node/"+link[0]
+
 
 s = ZhihuTopic()
-html = s.getTopList(253)
-#print(s.getTopSign(html));
-print(html)
+htmlText = s.getTopContent()
+print(s.getTopSignList(htmlText));
+
 
 
 

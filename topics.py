a78a5be0952ea78e7b5ed4fffed60a9cc8be7862
huaxiaofeng <huaxiaofeng@wanshifu.com>
2016-04-30 22:40:17 +0800

    增加话题的回答内容和赞同数,等等参数

--- topics.py
+++ topics.py
@@ -1,3 +1,5 @@
+#coding=utf-8
+
 '''
 知乎所有话题获取
 '''
@@ -18,12 +20,14 @@ class ZhihuTopic:
         self.requestMethod = 'POST'
 
     def getTopContent(self):
+        #获取知乎话题广场的html
         req = urllib.request.Request(self.topUrl)
         response = urllib.request.urlopen(req)
         htmlText = response.read()
         return htmlText
 
     def getTopSign(self, htmlText):
+        #获取所有的话题广场的所有话题(科技,运动,创业等)
         htmlText = htmlText.decode('UTF-8')
         searchObj = re.search(r'zg-icon-topic-square(.*)zm-topic-cat-sub', htmlText, re.M|re.I|re.S)
         if searchObj:
@@ -47,10 +51,9 @@ class ZhihuTopic:
         print(textDestination)
 
 
-    def getTopSignList(self, htmlText):
+    def getTopSignList(self, htmlText, offSet='0'):
         #通过ajax获取话题内的标签
         htmlText = htmlText.decode('UTF-8')
-        offSet = '0'
         xsrf = re.findall(r'name=\"_xsrf\" value=\"(.*)\"',htmlText,re.I)
         #获取请求参数
         param = re.findall(r'class=\"zh-general-list clearfix\" data-init=\"(.*)\">',htmlText,re.I)
@@ -69,12 +72,66 @@ class ZhihuTopic:
         textDestinationDict = json.loads(textDestination)
         if textDestinationDict['msg']:
             for textRow in textDestinationDict['msg']:
-                print (textRow)
+                urlLink = self.getSignLink (textRow)
+                self.getTopicTopAnswers(urlLink)
+                break
+
 
     def getSignLink(self, htmlText):
         #获取单个话题的链接地址
         link = re.findall(r'<a target=\"_blank\" href=\"(.*)\">',htmlText,re.I)
-        url = "https://www.zhihu.com/node/"+link[0]
+
+        urlLink = "https://www.zhihu.com"+link[0]+'/top-answers'
+
+        return urlLink
+
+
+    def getTopicTopAnswers(self, urlLink, page='1'):
+        #获取话题的精华题目和答案
+        print (urlLink)
+        urlLink = urlLink+'?page='+page
+        req = urllib.request.Request(urlLink)
+        response = urllib.request.urlopen(req)
+        htmlText = response.read().decode('UTF-8')
+        contentText = re.findall(r'<div class=\"feed-item feed-item-hook folding(.*?)<button class=\"meta-item item-collapse js-collapse\">', htmlText, re.I|re.M|re.S)
+        for signText in contentText:
+            self.getAnswerContent(signText)
+            break;
+
+    def getAnswerContent(self, htmlText):
+        #获取回答内容里面的问题相关信息
+        answerContent = re.findall(r'<textarea hidden class=\"content\">(.*)<\/textarea>', htmlText, re.M|re.S)  #回答内容
+
+        #赞同数量
+        voteCount = re.findall(r'data-votecount=\"(.*?)\">', htmlText, re.I);
+
+        #评论数
+        comment = re.findall(r'<i class=\"z-icon-comment\"><\/i>(.*)<\/a>', htmlText, re.I);
+        commentCount = comment[0].replace('条评论', '')
+
+        #问题的回答总数
+
+
+        #问题的标题
+        questionText = re.findall(r'<a class=\"question_link\" target=\"_blank\" href=\"(.*)\">(.*)<\/a>', htmlText, re.I|re.M)
+        questionTitle = questionText[0][1];
+
+        #问题的ID
+        questionId = questionText[0][0].replace('/question/', '')
+
+
+        #回答的ID
+        answerText = re.findall(r'<meta itemprop=\"answer-url-token\" content=\"(.*)\">', htmlText, re.I)
+        answerId = answerText[0]
+
+
+        print(answerContent)
+        print(voteCount)
+        print(commentCount)
+        print(questionTitle)
+        print(answerId)
+
+        print(questionId)
 
 
 s = ZhihuTopic()
